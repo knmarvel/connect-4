2548dd2b70c431bbd1b52ee8eb18d50434da0856
Karen Marvel <knmarvel@gmail.com>
2019-09-16 11:44:22 -0400

    Fixed columns/rows layout
    Co-authored-by: Detrich Schilling <detrich2009@hotmail.com>
    Co-authored-by: Featheringill <LeighAnnFeatheringill@gmail.com>

---
+++ js/jsknm.js
@@ -0,0 +1,27 @@
+//Make a click handler for each column "target or current target"
+    //don't add more when column is full
+        //if/return
+//Alternate between red and black discs
+//Filler for discs (CSS)
+//method to check positioning
+//way to go looping through these strings
+    //array that updates with the board with strings, start with "NNNNNNN", B for black, R for red
+    //foreach to loop through all the rows
+    //Declare an array
+        //All elements start with
+
+//Win conditions
+    //4 vertically:
+    // Slice the same character from each string, if they all match, win!
+    // 4 horizontally: 
+    // Does the string include RRRR or BBBB? If so, win! (kata 4:19)
+    // 4 diagonally:
+        // Slope of a line: y = mx+b
+        // X = row coordinate
+        // Y = column coordinate
+        // B = offset (0-2)
+        // We know slope: 1&-1
+        // (y-b)/x = 1 || -1
+
+//Win output "red wins!" "black wins" "ties" "stalemate"
+//
