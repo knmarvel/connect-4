9bfc2a0aab01e71fa237cb3aabccecc016ea3153
Karen Marvel <knmarvel@gmail.com>
2019-09-16 12:31:01 -0400

    fixed row layout Co-authored-by: Detrich Schilling <detrich2009@hotmail.com> Co-authored-by: Featheringill <LeighAnnFeatheringill@gmail.com>

--- js/jsknm.js
+++ js/jsknm.js
@@ -1,6 +1,7 @@
 //Make a click handler for each column "target or current target"
     //don't add more when column is full
         //if/return
+    //fill bottom open position (use get first element?)
 //Alternate between red and black discs
 //Filler for discs (CSS)
 //method to check positioning
@@ -8,12 +9,23 @@
     //array that updates with the board with strings, start with "NNNNNNN", B for black, R for red
     //foreach to loop through all the rows
     //Declare an array
-        //All elements start with
+        //All elements displayed in a string: 
+    //On click of column, we change the last remaining null in the string to appropriate color
+    //
+const columns = [
+    NNNNNN, //col 0: on click, the last null in the string changes to the appropriate color
+    NNNNNN, //col 1
+    NNNNNN, // col 2
+    NNNNNN, //col 3
+    NNNNNN, //col 4
+    NNNNNN, //col 5
+    NNNNNN //col 6
+]
 
 //Win conditions
-    //4 vertically:
+    //4 horizontally:
     // Slice the same character from each string, if they all match, win!
-    // 4 horizontally: 
+    // 4 vertically: 
     // Does the string include RRRR or BBBB? If so, win! (kata 4:19)
     // 4 diagonally:
         // Slope of a line: y = mx+b
